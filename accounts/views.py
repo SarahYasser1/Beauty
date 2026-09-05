d54f5a696afb1fa917bc7097b623179d86db6469
SarahYasser1 <12134455sy@gmail.com>
2026-09-05 22:47:38 +0300

    views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,26 @@
-# accounts/views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
 from .models import StudentProfile
+from .forms import RegisterForm
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            StudentProfile.objects.create(user=user)
+            login(request, user)
+            return redirect('course_list')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
 
 
 @login_required
