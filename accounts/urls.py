5587051141c3738e6afea5c59705d118cb76bbde
SarahYasser1 <12134455sy@gmail.com>
2026-09-05 22:48:06 +0300

    urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
-# accounts/urls.py
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
+    path('register/', views.register_view, name='register'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='course_list'), name='logout'),
     path('profile/', views.profile_view, name='profile'),
 ]
